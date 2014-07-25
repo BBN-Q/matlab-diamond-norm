99852b8bb3e7033671ecbb184e59eeb1d9dc10ae
Marcus P S <msilva@bbn.com>
2014-07-25 15:04:08 -0400

    Update README and make dnormtest a function

--- src/dnormtest.m
+++ src/dnormtest.m
@@ -1,3 +1,24 @@
+function dnormtest()
+%DNORMTEST Test for diamond norm function
+%
+%   dnormtest() runs a series of minimal tests on the functionality of the diamond norm.
+%
+%
+%  Copyright 2014 Raytheon BBN Technologies
+%
+%  Licensed under the Apache License, Version 2.0 (the "License");
+%  you may not use this file except in compliance with the License.
+%  You may obtain a copy of the License at
+%
+%      http://www.apache.org/licenses/LICENSE-2.0                                  
+%
+%  Unless required by applicable law or agreed to in writing, software
+%  distributed under the License is distributed on an "AS IS" BASIS,
+%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%  See the License for the specific language governing permissions and
+%  limitations under the License.
+%
+
 import qip.*
 import qip.open_systems.*
 
@@ -34,5 +55,6 @@ if norm(t - [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, sum(abs(r1-r2))/2]','inf') < 1e-11
 else
   disp('Test failed.');
 end
+
 
 
